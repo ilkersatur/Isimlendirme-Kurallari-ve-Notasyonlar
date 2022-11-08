923701533046dcad1dcbda42c9cc1ce058817385
İlker <lker@DESKTOP-DH0LVGV>
2022-11-08 21:58:59 +0300

    Add project files.

---
+++ Temel_programlama_11.10.2022/Program.cs
@@ -0,0 +1,41 @@
+﻿// Açıklama Satırı
+
+// Compiler açıklama satırlarını çalıştırmaz!
+// CTRL+K+C ile de olabilir. CTRL+K+U ile geri alır.
+/*
+   Açıklama satırı
+*/
+// KURAL 1 - C# programlama dili Case-Sensitive bir programlama dilidir.
+// A ! = a
+
+Console.WriteLine("String"); //cw tab+tab console.writeline kısayoludur.
+
+int sayi1; //ilk değeri yok
+int sayi2 = 10; // ilk değeri var
+int sayi3,sayi4;
+int sayi5=10, sayi6;
+
+Console.WriteLine(sayi2);
+Console.WriteLine("2.Örnek");
+
+//İsimlendirme Kuralları ve Notasyonlar
+/* int 8deneme; ilk karakterde sayı kullanılmaz
+ * int sayi 1; boşluk olmaz
+ * isimlendirmede isimler yapılan işle uygun olmalıdır.
+ */
+
+/* Notasyonlar
+ 
+1-macar 
+    ilk üç harfi kısaltma kullanılarak yapılır
+    btnKaydet, rdoÖdeme
+2-pascal
+    MaasHesapla   ----  maashesapla → bu kullanım okunması zordur
+3-camel
+    writeLine --- ilk harf küçük, 2. kelime büyük
+4-kebab
+    en-buyuk-sayi
+5-snake
+    en_buyuk_sayı
+*/
+Console.ReadLine(); //programı durdur
